90cfc323630f49a673a43c0ae7244585e5f9ae58
nick9083 <userf9083@gmail.com>
2025-11-23 05:42:28 +0900

    param

--- src/limo_drive/scripts/lkas_node.py
+++ src/limo_drive/scripts/lkas_node.py
@@ -37,7 +37,7 @@ class LKAS:
 
         # cmd_vel publisher
         self.ctrl_pub = rospy.Publisher("/cmd_vel_lkas", Twist, queue_size=1)
-        self.speed = 0.16
+        self.speed = 0.24
         self.trun_mutip = 0.14
 
         # 상태 변수들
