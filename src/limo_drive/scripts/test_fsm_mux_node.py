f18885986755a0ef0de49955c238f2969c6329e6
nick9083 <userf9083@gmail.com>
2025-12-15 02:02:06 +0900

    Add

--- src/limo_drive/scripts/test_fsm_mux_node.py
+++ src/limo_drive/scripts/test_fsm_mux_node.py
@@ -92,7 +92,7 @@ class FSMMuxNode:
 
         # mission5(1단계 턴) 조향값
         self.mission5_turn_speed = rospy.get_param("~mission5_turn_speed", 0.1)
-        self.mission5_turn_yaw   = rospy.get_param("~mission5_turn_yaw", -0.25)
+        self.mission5_turn_yaw   = rospy.get_param("~mission5_turn_yaw", -0.08)
 
         # v2x(2단계 턴) 조향값 (기존 파라미터 유지)
         self.v2x_turn_speed      = rospy.get_param("~v2x_turn_speed", 0.18)
