0928ca4764920ce805b97609767c435b0f487686
eunseo <kesmichaela893@naver.com>
2025-12-19 03:13:10 +0900

    hahaha

--- src/limo_drive/scripts/test_fsm_mux_node.py
+++ src/limo_drive/scripts/test_fsm_mux_node.py
@@ -84,7 +84,7 @@ class FSMMuxNode:
         # mission5: 첫 번째 v2x 턴이 시작되는 시점
         self.mission5_phase_time = rospy.get_param("~mission5_phase_time", 4)
         # v2x: 두 번째 턴이 시작되는 시점 (기존 v2x_phase_time 유지)
-        self.v2x_phase_time2    = rospy.get_param("~v2x_phase_time", 7)
+        self.v2x_phase_time2    = rospy.get_param("~v2x_phase_time", 6.3)
 
         # 두 턴에 *각각* 쓰는 턴 지속 시간
         self.mission5_turn_duration = rospy.get_param("~mission5_turn_duration", 0)
@@ -95,7 +95,7 @@ class FSMMuxNode:
         self.mission5_turn_yaw   = rospy.get_param("~mission5_turn_yaw", -0.00)
 
         # v2x(2단계 턴) 조향값 (기존 파라미터 유지)
-        self.v2x_turn_speed      = rospy.get_param("~v2x_turn_speed", 0.1)
+        self.v2x_turn_speed      = rospy.get_param("~v2x_turn_speed", 0.15)
         self.v2x_turn_yaw        = rospy.get_param("~v2x_turn_yaw", -0.13)
 
         # --- 상태 변수들 ---
